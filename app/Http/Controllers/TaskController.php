f97dd0723ffece06c454c945b62c7a4d1dd29c2d
J. B. Saige <jbspublic@gmail.com>
2016-10-01 12:37:18 -0600

    Created the task controller functions and views.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,4 +15,37 @@ class TaskController extends Controller
     {
         $this->middleware('auth');
     }
+
+    /**
+     * Display a list of all of the user's task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $tasks = $request->user()->tasks()->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Create a new task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create(['name' => $request->name,]);
+
+        return redirect('/tasks');
+    }
+
 }
