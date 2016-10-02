d3fccab3da716f97a0eb85599364af07926fd8e0
J. B. Saige <jbspublic@gmail.com>
2016-10-02 12:04:02 -0600

    Fixed issue #1.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -27,91 +27,91 @@
             </form>
         </div>
 
-        @if (count($tasks) > 0)
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Current Tasks
-                </div>
-
-                <div class="panel-body">
-
-                    <table id="task-table" class="table table-striped task-table" ng-controller="TaskListController">
-
-                        <thead>
-                            <tr>
-                                <th>Task</th>
-                                <th>Created</th>
-                                <th>&nbsp;</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <tr ng-repeat="task in tasks">
-
-                                <td class="table-text">
-                                    <div>@{{task.name}}</div>
-                                </td>
-
-                                <td class="table-text">
-                                    <div>@{{task.created_at}}</div>
-                                </td>
-
-                                <td>
-                                    <form id="form-delete-task-@{{task.id}}" onsubmit="return false;">
-                                        {{ csrf_field() }}
-                                        {{ method_field('DELETE') }}
-
-                                        <button type="button" id="button-delete-task_@{{task.id}}" onclick="deleteTask($(this).attr('id'))" class="btn btn-danger">
-                                            <i class="fa fa-btn fa-trash"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
             </div>
 
-            <script>
+            <div class="panel-body">
 
-                function deleteTask(id) {
-                    var ngController = angular.element($('#task-table')).scope();
-                    var ID = id.split('_')[1];
-                    var taskForm = $('#form-delete-task-' + ID);
-                    $.ajax({
-                        type: "POST",
-                        url: '/task/' + ID,
-                        data: taskForm.serialize(),
-                        success: function(data) {
-                            ngController.refresh();
-                        },
-                        error: function(jqXHR, textStatus, errorThrown) {
-                            alert('There was an error communicating with the server.');
-                        }
-                    });
-                }
+                <table id="task-table" class="table table-striped task-table" ng-controller="TaskListController">
 
-                function createTask() {
-                    var ngController = angular.element($('#task-table')).scope();
-                    var taskForm = $("#add-task-form");
-                    $.ajax({
-                        type: "POST",
-                        url: taskForm.attr('action'),
-                        data: taskForm.serialize(),
-                        success: function(data) {
-                            ngController.refresh();
-                            $("#task-name").val('');
-                        },
-                        error: function(jqXHR, textStatus, errorThrown) {
-                            alert('There was an error communicating with the server.');
-                        }
-                    });
-                }
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Created</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
 
-            </script>
+                    <tbody>
+                        <tr ng-repeat="task in tasks" ng-show="tasks.length">
 
-        @endif
+                            <td class="table-text">
+                                <div>@{{task.name}}</div>
+                            </td>
+
+                            <td class="table-text">
+                                <div>@{{task.created_at}}</div>
+                            </td>
+
+                            <td>
+                                <form id="form-delete-task-@{{task.id}}" onsubmit="return false;">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                    <button type="button" id="button-delete-task_@{{task.id}}" onclick="deleteTask($(this).attr('id'))" class="btn btn-danger">
+                                        <i class="fa fa-btn fa-trash"></i>Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        <tr ng-show="!tasks.length">
+                            <td colspan="3">There are no tasks in the task list.</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+
+        <script>
+
+            function deleteTask(id) {
+                var ngController = angular.element($('#task-table')).scope();
+                var ID = id.split('_')[1];
+                var taskForm = $('#form-delete-task-' + ID);
+                $.ajax({
+                    type: "POST",
+                    url: '/task/' + ID,
+                    data: taskForm.serialize(),
+                    success: function(data) {
+                        ngController.refresh();
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        alert('There was an error communicating with the server.');
+                    }
+                });
+            }
+
+            function createTask() {
+                var ngController = angular.element($('#task-table')).scope();
+                var taskForm = $("#add-task-form");
+                $.ajax({
+                    type: "POST",
+                    url: taskForm.attr('action'),
+                    data: taskForm.serialize(),
+                    success: function(data) {
+                        ngController.refresh();
+                        $("#task-name").val('');
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        alert('There was an error communicating with the server.');
+                    }
+                });
+            }
+
+        </script>
 
     </div>
 
